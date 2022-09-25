d3f691a5ddd0e7e281f38b4af1321ac2905ad664
etienne1698 <etienne.robert1698@gmail.com>
2022-09-25 12:29:27 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,6 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       "~": fileURLToPath(new URL("./src", import.meta.url)),
     },
+    dedupe: ["vue"],
   },
 });
