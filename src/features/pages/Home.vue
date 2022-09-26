c221b3a348a483b58bb8774f02d0d9f6eedff393
geoff <geoffroym2@gmail.com>
2022-09-27 00:20:52 +0200

    home

--- src/features/pages/Home.vue
+++ src/features/pages/Home.vue
@@ -12,6 +12,13 @@
     </Button>
     <Button type="submit"> Enregistrer </Button>
   </Form>
+  <Table
+    :items="[
+      { name: 'geoff', age: 23 },
+      { name: 'etienne', age: 24 },
+      { name: 'pierre', age: 32, status: 'nouille' },
+    ]"
+  ></Table>
 </template>
 
 <script setup lang="ts">
@@ -19,6 +26,7 @@ import { ref } from "vue";
 import Form from "../../core/components/form/Form.vue";
 import TextField from "../../core/components/form/TextField.vue";
 import Button from "../../core/components/Button.vue";
+import Table from "../../core/components/Table.vue";
 
 const displayTextField = ref(false);
 function a() {
