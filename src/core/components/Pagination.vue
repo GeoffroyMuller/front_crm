6436e9fc209bcf7426aff7cd654a26cd933ffcf3
geoff <geoffroym2@gmail.com>
2022-10-17 23:16:35 +0200

    up pagination

--- src/core/components/Pagination.vue
+++ src/core/components/Pagination.vue
@@ -1,29 +1,47 @@
 <template>
   <div class="pagination">
-    <Button class="btn-arrow" @click="internalCurrentPage--">{{ "<" }}</Button>
+    <Button class="btn-arrow" @click="substractOne">{{ "<" }}</Button>
     <TextField
       class="input-number-page"
       type="number"
+      :min="min"
+      :max="max"
       v-model="internalCurrentPage"
     />
-    <Button class="btn-arrow" @click="internalCurrentPage++">{{ ">" }}</Button>
+    <Button class="btn-arrow" @click="addOne">{{ ">" }}</Button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { withDefaults, defineProps, defineEmits, ref, watch } from "vue";
 import Button from "./Button.vue";
+import { isNil } from "lodash";
 import TextField from "./form/TextField.vue";
 
 interface PaginationProps {
   currentPage: number;
+  min?: number | undefined | null;
+  max?: number | undefined | null;
 }
 
 const props = withDefaults(defineProps<PaginationProps>(), {
   currentPage: 1,
+  min: 1,
+  max: null,
 });
 const emit = defineEmits(["update:currentPage"]);
 const internalCurrentPage = ref<number>(props.currentPage);
+
+const substractOne = () => {
+  if (isNil(props.min) || internalCurrentPage.value - 1 > props.min) {
+    internalCurrentPage.value--;
+  }
+};
+const addOne = () => {
+  if (isNil(props.max) || internalCurrentPage.value + 1 < props.max) {
+    internalCurrentPage.value++;
+  }
+};
 
 watch(
   () => internalCurrentPage.value,
