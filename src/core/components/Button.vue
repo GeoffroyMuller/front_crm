10090d76aaa8d0cad1cd998fe5b9cf0a5d021e06
etienne1698 <etienne.robert1698@gmail.com>
2022-10-21 22:20:28 +0200

    delete vieille transition

--- src/core/components/Button.vue
+++ src/core/components/Button.vue
@@ -96,23 +96,8 @@ const props = withDefaults(defineProps<ButtonProps>(), {
   color: #6b7280;
   position: relative;
   padding: 0;
-  overflow: hidden;
   &:hover {
     color: #4b5563;
-    &::before {
-      transform: translateX(0);
-      transition: transform 0.3s ease;
-    }
-  }
-  &::before {
-    width: 100%;
-    content: "";
-    transform: translateX(-100%);
-    position: absolute;
-    bottom: 0;
-    height: 1px;
-    background-color: #6b7280;
-    border-radius: 2px;
   }
 }
 
