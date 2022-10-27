95916c763fcecf875ae272304879e59e8a596f69
geoff <geoffroym2@gmail.com>
2022-10-27 07:35:50 +0200

    Pagination: up design & fix

--- src/core/components/Pagination.vue
+++ src/core/components/Pagination.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="pagination">
-    <Button class="btn-arrow" @click="substractOne">{{ "<" }}</Button>
-    <TextField
-      class="input-number-page"
-      type="number"
-      :appearance-none="true"
-      :min="min"
-      :max="max"
-      v-model="internalCurrentPage"
-    />
-    <Button class="btn-arrow" @click="addOne">{{ ">" }}</Button>
+    <div class="pagination-number-page">
+      <TextField
+        class="input-number-page"
+        type="number"
+        :appearance-none="true"
+        :min="min"
+        :max="max"
+        v-model="internalCurrentPage"
+      />
+      <span v-if="!isNil(max)">/ {{ max }}</span>
+    </div>
+
+    <IconButton name="chevron_left" @click.stop="substractOne()" />
+    <IconButton name="chevron_right" @click.stop="addOne()" />
   </div>
 </template>
 
@@ -18,6 +22,7 @@ import { withDefaults, defineProps, defineEmits, ref, watch } from "vue";
 import Button from "./Button.vue";
 import { isNil } from "lodash";
 import TextField from "./form/TextField.vue";
+import IconButton from "./IconButton.vue";
 
 interface PaginationProps {
   currentPage: number;
@@ -28,18 +33,18 @@ interface PaginationProps {
 const props = withDefaults(defineProps<PaginationProps>(), {
   currentPage: 1,
   min: 1,
-  max: null,
+  max: 10,
 });
 const emit = defineEmits(["update:currentPage"]);
 const internalCurrentPage = ref<number>(props.currentPage);
 
 const substractOne = () => {
-  if (isNil(props.min) || internalCurrentPage.value - 1 > props.min) {
+  if (isNil(props.min) || internalCurrentPage.value - 1 >= props.min) {
     internalCurrentPage.value--;
   }
 };
 const addOne = () => {
-  if (isNil(props.max) || internalCurrentPage.value + 1 < props.max) {
+  if (isNil(props.max) || internalCurrentPage.value + 1 <= props.max) {
     internalCurrentPage.value++;
   }
 };
@@ -56,10 +61,16 @@ watch(
 .pagination {
   display: flex;
   justify-content: space-between;
-  width: 140px;
+  align-items: center;
+  width: 160px;
+}
+.pagination-number-page {
+  display: flex;
+  align-items: center;
+  gap: 8px;
 }
 .input-number-page {
-  width: 4rem;
+  width: 3rem;
 }
 .input-number-page:deep(.input-class) {
   text-align: center;
