830a66f9714746d616790856815ca8bc7f97afbc
geoff <geoffroym2@gmail.com>
2022-11-10 15:19:25 +0100

    up form with yup

--- src/features/pages/customers/Edit.vue
+++ src/features/pages/customers/Edit.vue
@@ -1,74 +1,9 @@
 <template>
   <Card id="edit-customer-page" title="Edit Customer Form" v-if="isPageLoaded">
-    <Form :defaultValue="customer" @submit="handleSubmit">
-      <template v-slot:default="{ hasError }">
-        <TextField name="firstname" label="Prenom" :rules="[$rules.required()]" />
-        <TextField name="lastname" label="Nom" :rules="[$rules.required()]" />
-        <TextField name="email" type="email" label="Email" :rules="[$rules.required()]" />
-
-        <TextField name="description" label="Description" multiline />
-
-        <Select
-          name="gender"
-          label="Genre"
-          :options="[
-            { label: 'Monsieur', value: 'M' },
-            { label: 'Madame', value: 'Mme' },
-          ]"
-          :rules="[$rules.required()]"
-        />
-
-        <DatePicker
-          name="birthdate"
-          label="Date de naissance"
-          :rules="[$rules.required()]"
-        />
-
-        <Autocomplete
-          label="Ville"
-          name="city"
-          auto-filter
-          :rules="[$rules.required()]"
-          :options="[
-            { label: 'Metz', value: 'Metz' },
-            { label: 'Paris', value: 'Paris' },
-            { label: 'Nancy', value: 'Nancy' },
-            { label: 'Noisseville', value: 'Noisseville' },
-            { label: 'Lyon', value: 'Lyon' },
-          ]"
-        />
-
-        <RadioGroup
-          label="Valide"
-          name="valid"
-          :options="[
-            { label: 'Oui', value: true },
-            { label: 'Non', value: false },
-          ]"
-          :rules="[$rules.required()]"
-        />
-
-        <Switch label="Authorisé" name="authorized" />
-
-        <Button
-          v-if="isAddAction"
-          type="submit"
-          :loading="loading"
-          :disabled="hasError"
-          icon="save"
-        >
-          Ajouter
-        </Button>
-        <Button v-else type="submit" :loading="loading" :disabled="hasError" icon="save">
-          Modifier
-        </Button>
-
-        <Button @click.stop="sidebarOpen = true">Open sidebar</Button>
-        <Sidebar v-model:open="sidebarOpen">
-          <CustomerForm :customerId="customer?.id" />
-        </Sidebar>
-      </template>
-    </Form>
+    <Button @click.stop="sidebarOpen = true">Open sidebar</Button>
+    <Sidebar v-model:open="sidebarOpen">
+      <CustomerForm :customerId="customer?.id" />
+    </Sidebar>
   </Card>
 </template>
 
@@ -77,31 +12,21 @@ import { computed, ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { useCustomerStore } from "@/features/stores/customers";
 import Card from "@/core/components/Card.vue";
-import TextField from "@/core/components/form/TextField.vue";
-import Form from "@/core/components/form/Form.vue";
 import { isNil } from "lodash";
-import Button from "../../../core/components/Button.vue";
 import usePage from "@/features/composables/page";
-import { useRouter } from "vue-router";
 import useUI from "@/core/helpers/vue/composables/ui";
-import Select from "@/core/components/form/Select.vue";
-import DatePicker from "@/core/components/form/DatePicker.vue";
-import Autocomplete from "@/core/components/form/Autocomplete.vue";
-import RadioGroup from "@/core/components/form/RadioGroup.vue";
-import Switch from "@/core/components/form/Switch.vue";
 import Sidebar from "@/core/components/Sidebar.vue";
 import CustomerForm from "../../components/CustomerForm.vue";
+import Button from "../../../core/components/Button.vue";
 
 const customerStore = useCustomerStore();
-
-const router = useRouter();
 const route = useRoute();
 
 const isAddAction = computed(() => !route.params.id);
 
-const isPageLoaded = computed(() => isAddAction.value || !isNil(customer.value));
-
-const loading = ref(false);
+const isPageLoaded = computed(
+  () => isAddAction.value || !isNil(customer.value)
+);
 const sidebarOpen = ref(false);
 
 const customer = computed(() => {
@@ -134,35 +59,6 @@ onMounted(async () => {
   await customerStore.fetchById(route.params.id);
   setPageLoading(false);
 });
-
-async function handleSubmit(data: any) {
-  loading.value = true;
-  try {
-    if (isAddAction.value) {
-      await customerStore.create(data);
-      toast({
-        type: "success",
-        message: `Utilisateur créé avec succes`,
-      });
-    } else {
-      await customerStore.update(route.params.id, data);
-      toast({
-        type: "success",
-        message: `Utilisateur <b>${customer.value.firstname}${
-          customer.value.lastname ? " " + customer.value.lastname : ""
-        }</b> modifié avec succes`,
-      });
-      router.push("/customers");
-    }
-  } catch (err) {
-    console.error(err);
-    toast({
-      type: "danger",
-      message: `Erreur lors de la modification`,
-    });
-  }
-  loading.value = false;
-}
 </script>
 
 <style lang="scss" scoped>
