4ae026eab37f94f98f243e1679771512f8e8c015
geoff <geoffroym2@gmail.com>
2022-11-11 18:07:26 +0100

    add List components

---
+++ src/core/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="list">
+    <ul>
+      <li v-for="(item, index) in list" :key="index">
+        <p v-if="!$slots.default">
+          {{ getLabel ? getLabel(item) : item }}
+        </p>
+        <slot v-else :item="item" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface ListProps {
+  list: Array<any> | null;
+  getLabel?: (item: any) => string;
+}
+const props = withDefaults(defineProps<ListProps>(), {});
+</script>
+
+<style lang="scss" scoped>
+li {
+  list-style-type: none;
+}
+</style>
--- src/features/pages/Home.vue
+++ src/features/pages/Home.vue
@@ -1,8 +1,24 @@
 <template>
   <div>{{ $t("WELCOME") }}</div>
   <Chip isClosable isOutline color="primary" icon="expand_more">Chip</Chip>
+  <List
+    :list="[
+      { titre: 'yo', poui: 'uuuu' },
+      { titre: 'dq', poui: 'ppppp' },
+    ]"
+    :getLabel="
+      (item) => {
+        return item.titre;
+      }
+    "
+  >
+    <template #default="{ item }">
+      <div>- {{ item.poui }}</div>
+    </template>
+  </List>
 </template>
 
 <script setup lang="ts">
 import Chip from "@/core/components/Chip.vue";
+import List from "@/core/components/List.vue";
 </script>
