db1d08e77971805c7f2efe3d4697176dfdea9909
geoff <geoffroym2@gmail.com>
2022-11-14 16:53:37 +0100

    exemple tooltip

--- src/features/pages/Home.vue
+++ src/features/pages/Home.vue
@@ -1,24 +1,35 @@
 <template>
-  <div>{{ $t("WELCOME") }}</div>
-  <Chip isClosable isOutline color="primary" icon="expand_more">Chip</Chip>
-  <List
-    :list="[
-      { titre: 'yo', poui: 'uuuu' },
-      { titre: 'dq', poui: 'ppppp' },
-    ]"
-    :getLabel="
-      (item) => {
-        return item.titre;
-      }
-    "
-  >
-    <template #default="{ item }">
-      <div>- {{ item.poui }}</div>
-    </template>
-  </List>
+  <div class="home">
+    <Button v-tooltip="{ text: 'My Tooltip', placement: 'bottom' }">
+      Tooltip
+    </Button>
+    <Chip isClosable isOutline color="primary" icon="expand_more">Chip</Chip>
+    <List
+      :list="[
+        { titre: 'yo', poui: 'uuuu' },
+        { titre: 'dq', poui: 'ppppp' },
+      ]"
+      :getLabel="
+        (item) => {
+          return item.titre;
+        }
+      "
+    >
+      <template #default="{ item }">
+        <div>- {{ item.poui }}</div>
+      </template>
+    </List>
+  </div>
 </template>
 
 <script setup lang="ts">
+import Button from "@/core/components/Button.vue";
 import Chip from "@/core/components/Chip.vue";
 import List from "@/core/components/List.vue";
 </script>
+
+<style lang="scss" scoped>
+.home {
+  @include flex(col, flex-start, center, 2);
+}
+</style>
