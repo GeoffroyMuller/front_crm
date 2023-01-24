dc259abe276670dd696548cff66898d6a651795b
geoff <geoffroym2@gmail.com>
2023-01-24 12:45:46 +0100

    wip SalesForm

--- src/components/sales/SalesForm.vue
+++ src/components/sales/SalesForm.vue
@@ -11,16 +11,100 @@
           :get-filters="(str) => ({ $contains: { firstname: str } })"
           :store="clientStore"
         />
-      </div>
-      <div class="actions">
-        <Button
-          type="submit"
-          color="success"
-          icon="add"
-          :disabled="hasError || !hasChanged"
+
+        <Repetable
+          :label="$t('pages.edit-sale.add-products')"
+          name="sales_products"
         >
-          {{ isNew ? $t("add") : $t("save") }}
-        </Button>
+          <template #default="{ data: dataProduct }">
+            <Grid :gap="2" :columns="1">
+              <Grid :gap="2" :columns="2">
+                <MagicAutocomplete
+                  name="product"
+                  :label="$t('product')"
+                  :getOptionLabel="displayProductAutocomplete"
+                  :get-filters="
+                    (str) => ({ $or: { $contains: { name: str, price: str } } })
+                  "
+                  :store="productStore"
+                />
+                <template v-if="dataProduct.product">
+                  <Grid :gap="2" :columns="2">
+                    <TextField
+                      v-if="!productStore.isPhysicalStock(dataProduct.product)"
+                      :rules="$yup.string().required()"
+                      :model-value="dataProduct.product?.price"
+                      type="number"
+                      :step="0.01"
+                      name="price"
+                      :label="$t('unit-price')"
+                    />
+                    <TextField
+                      v-if="productStore.isNumeraryStock(dataProduct.product)"
+                      :rules="$yup.string().required()"
+                      :model-value="1"
+                      :min="1"
+                      type="number"
+                      :step="1"
+                      name="quantity"
+                      :label="$t('quantity')"
+                    />
+                  </Grid>
+                </template>
+              </Grid>
+              <Repetable
+                v-if="productStore.isPhysicalStock(dataProduct.product)"
+                name="sales_products_real"
+              >
+                <template #default>
+                  <Grid :gap="2" :columns="2">
+                    <MagicAutocomplete
+                      name="products_real"
+                      :label="$t('reference')"
+                      :getOptionLabel="displayProductRealAutocomplete"
+                      :get-filters="
+                        (str) => ({
+                          $contains: {
+                            reference: str,
+                            idProduct: dataProduct.product.id,
+                          },
+                        })
+                      "
+                      :store="productRealStore"
+                    />
+                    <Grid :gap="2" :columns="2">
+                      <TextField
+                        :rules="$yup.string().required()"
+                        :model-value="dataProduct.product?.price"
+                        type="number"
+                        :step="0.01"
+                        name="price"
+                        :label="$t('unit-price')"
+                      />
+                    </Grid>
+                  </Grid>
+                </template>
+              </Repetable>
+            </Grid>
+          </template>
+          <template #actions="{ addSection }">
+            <Button type="button" variant="text" @click="addSection()">
+              {{ $t("pages.edit-sale.add-product") }}
+            </Button>
+          </template>
+        </Repetable>
+        <div>
+          {{ $t("pages.edit-sale.total-price", { price: totalPrice }) }}
+        </div>
+        <div class="form-bottom">
+          <Button
+            type="submit"
+            :icon="isNew ? 'add' : 'save'"
+            :disabled="hasError || !hasChanged"
+          >
+            {{ isNew ? $t("create") : $t("save") }}
+          </Button>
+        </div>
       </div>
     </template>
   </Form>
@@ -29,13 +113,19 @@
 import Button from "@/core/components/Button.vue";
 import DataTable from "@/core/components/DataTable.vue";
 import Form from "@/core/components/form/Form.vue";
+import Repetable from "@/core/components/form/repetable/Repetable.vue";
+import TextField from "@/core/components/form/TextField.vue";
+import Grid from "@/core/components/layouts/Grid.vue";
 import MagicAutocomplete from "@/core/components/magic/MagicAutocomplete.vue";
 import useUI from "@/core/helpers/vue/composables/ui";
 import useClientStore from "@/stores/clients";
+import useProductsStore from "@/stores/products";
+import useProductsRealStore from "@/stores/products_real";
 import useSaleStore from "@/stores/sales";
-import type { Product } from "@/types/product";
+import type { Product, ProductReal } from "@/types/product";
 import type { Sale } from "@/types/sale";
 import type { ID } from "@/types/utils";
+import { isNil } from "lodash";
 import { computed, ref } from "vue";
 import { useI18n } from "vue-i18n";
 
@@ -52,15 +142,32 @@ const { toast } = useUI();
 const { t } = useI18n();
 const saleStore = useSaleStore();
 const clientStore = useClientStore();
+const productStore = useProductsStore();
+const productRealStore = useProductsRealStore();
 
 const isNew = computed(() => props.sale == null);
 
-const saleProducts = ref<Array<Product>>(
-  props.sale ? props.sale.products || [] : []
-);
+const totalPrice = computed(() => 10.0); //TODO
+
+function displayProductAutocomplete(product: Product): string {
+  return `${product.name} ${"| " + t("price") + " : " + product.price} ${
+    productStore.isNumeraryStock(product)
+      ? "| " + t("stock") + " : " + product.stock
+      : ""
+  }`;
+}
+function displayProductRealAutocomplete(productReal: ProductReal) {
+  return `${productReal.reference}`;
+}
+function _mapDataFormToSales(data: any): Sale {
+  return {
+    idCustomer: data.idCustomer,
+  };
+} //TODO
 
 async function handleSubmit(data: any) {
-  if (isNew.value) {
+  console.error({ data });
+  /*   if (isNew.value) {
     try {
       const newSale = await saleStore.create(data);
       emit("add", newSale);
@@ -89,7 +196,14 @@ async function handleSubmit(data: any) {
         message: error.response.data.message,
       });
     }
-  }
+  } */
 }
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.form-sales {
+  @include grid(1, 0, 2);
+}
+.form-bottom {
+  @include flex(row, flex-end, center, 2);
+}
+</style>
