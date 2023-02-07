0efd273410c94e555b291c301b1bf9e1bdba92ad
geoff <geoffroym2@gmail.com>
2023-02-07 20:19:30 +0100

    up

--- src/pages/invoices/Invoice.vue
+++ src/pages/invoices/Invoice.vue
@@ -148,8 +148,7 @@ const {
 <style lang="scss" scoped>
 .invoice-actions {
   padding: spacing(0.5) spacing(2);
-  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 10%), 0 1px 2px -1px rgb(0 0 0 / 10%);
-  /* background-color: color("primary", 50); */
+  background-color: color("primary", 50);
 }
 .invoice-content {
   padding: spacing(2);
