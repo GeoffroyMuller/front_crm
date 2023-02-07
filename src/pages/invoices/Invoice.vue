87921c996de0d7b5ca4d45609ec447bd48814588
geoff <geoffroym2@gmail.com>
2023-02-07 20:10:37 +0100

    up design invoice

--- src/pages/invoices/Invoice.vue
+++ src/pages/invoices/Invoice.vue
@@ -5,95 +5,100 @@
     :loading="!invoice"
   >
     <Grid :gap="1">
-      <!-- <pre class="code" :style="{ maxHeight: '220px' }">{{ invoice }}</pre> -->
-      <Flex :gap="1">
-        <Button @click="$router.push(`/invoices/${id}/edit`)" icon="edit">
-          {{ $t("edit") }}
-        </Button>
-        <Button @click="setArchived" icon="archive">
-          {{ $t("archive") }}
-        </Button>
-        <Button @click="downloadPdf" icon="download">
-          {{ $t("download") }}
-        </Button>
-        <Button @click="sendMail" icon="mail">
-          {{ $t("send_by_mail") }}
-        </Button>
-        <Button @click.stop="preview" icon="preview">
-          {{ $t("preview") }}
-        </Button>
-      </Flex>
-
-      <Card>
-        <Grid :gap="5" :columns="1">
-          <div class="subtitle">{{ invoice.name }}</div>
-          <Flex justify-content="space-between">
-            <Flex
-              direction="column"
-              justify-content="center"
-              align-items="flex-start"
-            >
-              <div class="invoice-user-title">{{ $t("responsible") }}:</div>
-              <div class="text2">
-                {{ invoice.responsible.firstname }}
-                {{ invoice.responsible.lastname }}
-              </div>
-              <div class="text2">
-                {{ invoice.responsible.email }}
-              </div>
-            </Flex>
-            <Flex
-              direction="column"
-              justify-content="center"
-              align-items="flex-end"
-            >
-              <div>
-                <div class="invoice-user-title">{{ $t("customer") }}:</div>
+      <Card :withPadding="false">
+        <Flex class="invoice-actions" :gap="1">
+          <Button
+            variant="text"
+            @click="$router.push(`/invoices/${id}/edit`)"
+            icon="edit"
+          >
+            {{ $t("edit") }}
+          </Button>
+          <Button variant="text" @click="setArchived" icon="archive">
+            {{ $t("archive") }}
+          </Button>
+          <Button variant="text" @click="downloadPdf" icon="download">
+            {{ $t("download") }}
+          </Button>
+          <Button variant="text" @click="sendMail" icon="mail">
+            {{ $t("send_by_mail") }}
+          </Button>
+          <Button variant="text" @click.stop="preview" icon="preview">
+            {{ $t("preview") }}
+          </Button>
+        </Flex>
+        <div class="invoice-content">
+          <Grid :gap="5" :columns="1">
+            <div class="subtitle">{{ invoice.name }}</div>
+            <Flex justify-content="space-between">
+              <Flex
+                direction="column"
+                justify-content="center"
+                align-items="flex-start"
+              >
+                <div class="invoice-user-title">{{ $t("responsible") }}:</div>
                 <div class="text2">
-                  {{ invoice.client?.firstname }} {{ invoice.client?.lastname }}
+                  {{ invoice.responsible.firstname }}
+                  {{ invoice.responsible.lastname }}
                 </div>
                 <div class="text2">
-                  {{ invoice.client?.address }}
+                  {{ invoice.responsible.email }}
                 </div>
-              </div>
+              </Flex>
+              <Flex
+                direction="column"
+                justify-content="center"
+                align-items="flex-end"
+              >
+                <div>
+                  <div class="invoice-user-title">{{ $t("customer") }}:</div>
+                  <div class="text2">
+                    {{ invoice.client?.firstname }}
+                    {{ invoice.client?.lastname }}
+                  </div>
+                  <div class="text2">
+                    {{ invoice.client?.address }}
+                  </div>
+                </div>
+              </Flex>
             </Flex>
-          </Flex>
 
-          <Grid :gap="1" :columns="1">
-            <div v-for="line in invoice.lines" :key="line.id">
-              <Card>
-                <Grid :gap="1" :columns="4">
-                  <Grid :gap="1" :columns="1">
-                    <div class="text">{{ $t("description") }}:</div>
-                    <div class="text2" v-html="line.description"></div>
+            <Grid :gap="1" :columns="1">
+              <div v-for="line in invoice.lines" :key="line.id">
+                <Card>
+                  <Grid :gap="1" :columns="4">
+                    <Grid :gap="1" :columns="1">
+                      <div class="text">{{ $t("description") }}:</div>
+                      <div class="text2" v-html="line.description"></div>
+                    </Grid>
+                    <Grid v-if="line.qty" :gap="1" :columns="1">
+                      <div class="text">{{ $t("quantity") }}:</div>
+                      <div class="text2">{{ line.qty }}</div>
+                    </Grid>
+                    <Grid v-if="line.unit_price" :gap="1" :columns="1">
+                      <div class="text">{{ $t("unit-price") }}:</div>
+                      <div class="text2">
+                        {{ $utils.formatPrice(line.unit_price!) }}
+                      </div>
+                    </Grid>
+                    <Grid v-if="line.vat" :gap="1" :columns="1">
+                      <div class="text">{{ $t("vat") }}:</div>
+                      <div class="text2">{{ line.vat.rate }}%</div>
+                    </Grid>
                   </Grid>
-                  <Grid v-if="line.qty" :gap="1" :columns="1">
-                    <div class="text">{{ $t("quantity") }}:</div>
-                    <div class="text2">{{ line.qty }}</div>
-                  </Grid>
-                  <Grid v-if="line.unit_price" :gap="1" :columns="1">
-                    <div class="text">{{ $t("unit-price") }}:</div>
-                    <div class="text2">
-                      {{ $utils.formatPrice(line.unit_price!) }}
-                    </div>
-                  </Grid>
-                  <Grid v-if="line.vat" :gap="1" :columns="1">
-                    <div class="text">{{ $t("vat") }}:</div>
-                    <div class="text2">{{ line.vat.rate }}%</div>
-                  </Grid>
-                </Grid>
-              </Card>
-            </div>
+                </Card>
+              </div>
+            </Grid>
+            <Grid v-if="invoice.modalities" :gap="1" :columns="1">
+              <div class="text">{{ $t("modalities") }}:</div>
+              <div class="text2" v-html="invoice.modalities"></div>
+            </Grid>
+            <Grid v-if="invoice.footer" :gap="1" :columns="1">
+              <div class="text">{{ $t("footer") }}:</div>
+              <div class="text2" v-html="invoice.footer"></div>
+            </Grid>
           </Grid>
-          <Grid v-if="invoice.modalities" :gap="1" :columns="1">
-            <div class="text">{{ $t("modalities") }}:</div>
-            <div class="text2" v-html="invoice.modalities"></div>
-          </Grid>
-          <Grid v-if="invoice.footer" :gap="1" :columns="1">
-            <div class="text">{{ $t("footer") }}:</div>
-            <div class="text2" v-html="invoice.footer"></div>
-          </Grid>
-        </Grid>
+        </div>
       </Card>
 
       <InvoicePayments :invoice="invoice" />
@@ -141,8 +146,15 @@ const {
 } = useInvoice({ invoice });
 </script>
 <style lang="scss" scoped>
-.invoice-user-title {
-  @include typo(text);
-  margin-bottom: spacing(1);
+.invoice-actions {
+  padding: spacing(0.5) spacing(2);
+  background-color: color("primary", 50);
+}
+.invoice-content {
+  padding: spacing(2);
+  .invoice-user-title {
+    @include typo(text);
+    margin-bottom: spacing(1);
+  }
 }
 </style>
