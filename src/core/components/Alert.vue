94d2b383e757a7e002ed12741aedccd6f9453b52
geoff <geoffroym2@gmail.com>
2023-02-07 17:14:25 +0100

    add color alert

--- src/core/components/Alert.vue
+++ src/core/components/Alert.vue
@@ -1,20 +1,36 @@
 <template>
-  <div class="alert">
+  <div
+    class="alert"
+    :class="{
+      [`alert-${color}`]: color,
+    }"
+  >
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Color } from "./types";
+
 interface AlertProps {
   error?: string;
+  color?: Color;
 }
-const props = withDefaults(defineProps<AlertProps>(), {});
+const props = withDefaults(defineProps<AlertProps>(), {
+  color: "danger",
+});
 </script>
 
 <style lang="scss">
 .alert {
-  color: color("danger", 500);
   width: fit-content;
   max-width: 100%;
+}
+@each $key, $value in $colors {
+  @if type-of($value) == "map" {
+    .alert-#{$key} {
+      color: map-deep-get($value, 500);
+    }
+  }
 }
 </style>
