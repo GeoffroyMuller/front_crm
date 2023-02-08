b9bd30aae7ee380f77be5b1f51900cdec73f028d
etienne1698 <etienne.robert1698@gmail.com>
2023-02-09 00:24:33 +0100

    update

--- src/core/components/form/EmojiPicker.vue
+++ src/core/components/form/EmojiPicker.vue
@@ -40,25 +40,42 @@ export interface EmojiPickerProps {
 function generateEmojis() {
   let res: Array<string> = [];
   const emojRange = [
-    [parseInt("1F600", 16), 128591],
-    [9986, 10160],
-    [128640, 128704],
+    [128512, 128567], // head
+    [128577, 128580], // head
+    [129296, 129303], // head
+    [129312, 129327], // head
+    [129488, 129488], // head
+    [128066, 128080], // body
+    [129304, 129305], // body
   ];
-  function generateUtf8Characters(range: Array<number>) {
+  function generateEmojisCode(
+    range0: number,
+    range1: number,
+    withSkinTones?: boolean
+  ) {
     const codes = [];
-    for (let i = range[0]; i < range[1]; i++) {
+    for (let i = range0; i <= range1; i++) {
       codes.push(String.fromCodePoint(i));
+      /* if (withSkinTones) {
+        codes.push(String.fromCodePoint(i) + String.fromCodePoint(127999));
+        codes.push(String.fromCodePoint(i) + String.fromCodePoint(127998));
+        codes.push(String.fromCodePoint(i) + String.fromCodePoint(127997));
+        codes.push(String.fromCodePoint(i) + String.fromCodePoint(127996));
+        codes.push(String.fromCodePoint(i) + String.fromCodePoint(127995));
+      } */
     }
     return codes;
   }
   for (const range of emojRange) {
-    res = [...res, ...generateUtf8Characters(range)];
+    // @ts-ignore
+    res = [...res, ...generateEmojisCode(range[0], range[1], range?.[2])];
   }
   return res;
 }
 
 const emojis = generateEmojis();
 
+console.error(emojis.length);
 const props = withDefaults(defineProps<EmojiPickerProps>(), {});
 
 const emit = defineEmits(["update:modelValue"]);
