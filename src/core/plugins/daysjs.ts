191e733736185dc579cadafe991c2f75fd4e303f
etienne1698 <etienne.robert1698@gmail.com>
2023-02-19 23:31:43 +0100

    up plugins

--- src/core/plugins/daysjs.ts
+++ src/core/plugins/daysjs.ts
@@ -1,8 +1,10 @@
 import dayjs from "dayjs";
 import localeData from "dayjs/plugin/localeData";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 import "dayjs/locale/fr";
 
 dayjs.extend(localeData);
+dayjs.extend(customParseFormat);
 
 dayjs.locale("fr");
