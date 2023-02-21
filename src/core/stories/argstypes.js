20ba53b93e7f8eb95ac154442a601a251c5f8005
etienne1698 <etienne.robert1698@gmail.com>
2023-02-21 23:50:04 +0100

    storybook

---
+++ src/core/stories/argstypes.js
@@ -0,0 +1,10 @@
+export default {
+  color: {
+    control: { type: "select" },
+    options: ["primary", "danger", "success", "warning", "white", "black"],
+  },
+  icon: {
+    control: { type: "select" },
+    options: ["home", "person"],
+  },
+};
