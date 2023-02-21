744e001c8c269df8830e5291f95ad65a40878f25
etienne1698 <etienne.robert1698@gmail.com>
2023-02-21 21:56:06 +0100

    types

--- src/core/plugins/typescript.ts
+++ src/core/plugins/typescript.ts
@@ -1,11 +1,12 @@
 import type { LoDashStatic } from "lodash";
 import type { $breakpoints } from "./breakpoints";
 import type { $utils } from "./utils";
+import type * as yup from "yup";
 
 declare module "vue" {
   interface ComponentCustomProperties {
     $_: LoDashStatic;
-    $yup: any;
+    $yup: typeof yup;
     $utils: $utils;
     $breakpoints: $breakpoints;
   }
