a6592703cfdaf1cc5370f0da3e1e699ecabd3cd6
etienne1698 <etienne.robert1698@gmail.com>
2023-03-02 14:17:46 +0100

    up

--- cli/generate-keypair.mjs
+++ cli/generate-keypair.mjs
@@ -1,6 +1,7 @@
 import NodeRSA from "node-rsa";
 import * as fs from "fs"; 
 import path from "path";
+import chalk from 'chalk';
 
 
 const key = new NodeRSA({ b: 2048 });
@@ -9,8 +10,8 @@ let keypair = {
   public: key.exportKey("public")
 };
 
-console.log(keypair);
 
+/* 
 fs.writeFileSync(
   path.join("packages", "auth_service", "private.key"),
   keypair.private
@@ -24,4 +25,11 @@ fs.writeFileSync(
 fs.writeFileSync(
   path.join("packages", "core_api", "public.key.pub"),
   keypair.public
-);
+); */
+
+console.log(chalk.yellow('PUBLIC KEY:'));
+console.log(chalk.blue(keypair.public));
+console.log(chalk.yellow('PRIVATE KEY:'));
+console.log(chalk.blue(keypair.private));
+
+console.log(chalk.green('Keys generated successfully!'));
