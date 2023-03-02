1e219425fde78f491808c4108934408b84be488f
geoff <geoffroym2@gmail.com>
2023-03-02 13:24:47 +0100

    up qty in resa

--- packages/erp/src/components/reservations/ReservationProductLine.vue
+++ packages/erp/src/components/reservations/ReservationProductLine.vue
@@ -1,19 +1,29 @@
 <template>
-  <div>
+  <div class="reservation-product-line">
     <Grid :gap="2" :columns="1">
-      <MagicAutocomplete
-        name="product"
-        :getOptionValue="(opt) => opt"
-        :label="$t('product')"
-        :getOptionLabel="displayProductAutocomplete"
-        @update:selected="handleProductChange"
-        :get-filters="
-          (str) => ({ $or: { $contains: { name: str, price: str } } })
-        "
-        :store="productStore"
-      />
+      <Grid :gap="2" :columns="3">
+        <MagicAutocomplete
+          class="reservation-product"
+          name="product"
+          :getOptionValue="(opt) => opt"
+          :label="$t('product')"
+          :getOptionLabel="displayProductAutocomplete"
+          @update:selected="handleProductChange"
+          :get-filters="
+            (str) => ({ $or: { $contains: { name: str, price: str } } })
+          "
+          :store="productStore"
+        />
+        <TextField
+          class="input"
+          type="number"
+          :label="$t('pages.edit-quote.qty')"
+          :step="1"
+          name="qty"
+        />
+      </Grid>
 
-      <Repetable
+      <!-- <Repetable
         v-if="productStore.isPhysicalStock(product) && product"
         name="sublines"
       >
@@ -27,7 +37,7 @@
             {{ $t("pages.edit-reservation.add-product-real") }}
           </Button>
         </template>
-      </Repetable>
+      </Repetable> -->
     </Grid>
   </div>
 </template>
@@ -42,6 +52,7 @@ import type { SaleLine } from "@/types/sale";
 import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 import ReservationProductRealLine from "./ReservationProductRealLine.vue";
+import TextField from "core/src/components/form/TextField.vue";
 
 const { t } = useI18n();
 const productStore = useProductsStore();
@@ -49,7 +60,6 @@ const productStore = useProductsStore();
 interface ReservationProductLineProps {
   line?: SaleLine;
 }
-
 const props = withDefaults(defineProps<ReservationProductLineProps>(), {});
 const product = ref<Product | null>();
 
@@ -65,4 +75,10 @@ function handleProductChange(_product: Product) {
   product.value = _product;
 }
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.reservation-product-line {
+  .reservation-product {
+    grid-column: 1 / 3;
+  }
+}
+</style>
