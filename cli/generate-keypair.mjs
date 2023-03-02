87bf6bc94be6ea266857499f06e054b318eba70a
geoff <geoffroym2@gmail.com>
2023-03-02 21:01:05 +0100

    up

--- cli/generate-keypair.mjs
+++ cli/generate-keypair.mjs
@@ -11,12 +11,12 @@ let keypair = {
 };
 
 fs.writeFileSync(
-  path.join("packages", "auth_api", "private.key"),
+  path.join("packages", "api_auth", "private.key"),
   keypair.private
 );
 
 fs.writeFileSync(
-  path.join("packages", "auth_api", "public.key.pub"),
+  path.join("packages", "api_auth", "public.key.pub"),
   keypair.public
 );
 
