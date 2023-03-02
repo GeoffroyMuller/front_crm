eb7f6bed08f09837b6e4ea1d88dc37cc386c4907
etienne1698 <etienne.robert1698@gmail.com>
2023-03-02 22:08:01 +0100

    script start apis

---
+++ cli/start-apis.mjs
@@ -0,0 +1,27 @@
+import chalk from "chalk";
+import { exec, spawn } from "child_process";
+import * as fs from "fs";
+import path from "path";
+
+const print = console.log;
+const stdout = (data, folder) => print(`[${folder}]:${data}`);
+const stderr = (data, folder) => print(chalk.red(`[${folder}]:stderr: ${data}`));
+const error = (data) => print(chalk.red(`error: ${data.message}`));
+const close = (code) =>
+  print(chalk.yellow(`child process exited with code ${code}`));
+
+fs.readdir(path.join("packages"), (err, foldersNames) => {
+  if (!err) {
+    foldersNames.forEach((folder) => {
+        if (folder.startsWith("api_")) {
+            const childProcess = exec(`cd ./packages/${folder} & yarn dev`);
+            childProcess.stdout.on("data", data => stdout(data, folder));
+            childProcess.stderr.on("data", data => stderr(data, folder));
+            childProcess.on("error", error);
+            childProcess.on("close", close);
+        }
+    });
+  }
+});
+
+
