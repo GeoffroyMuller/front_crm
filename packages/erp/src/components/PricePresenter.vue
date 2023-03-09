5dda6e8aafc4581ec0a9b36388df3ef0b5b09c3e
etienne1698 <etienne.robert1698@gmail.com>
2023-03-09 16:16:52 +0100

    style

--- packages/erp/src/components/PricePresenter.vue
+++ packages/erp/src/components/PricePresenter.vue
@@ -14,14 +14,26 @@ const props = withDefaults(defineProps<PricePresenterProps>(), {});
 
 <style lang="scss">
 .price-presenter {
-  border-radius: map-deep-get($rounded, "md");
+  border-radius: map-deep-get($rounded, "sm");
   padding: spacing(0.5);
   color: color("white");
+
   &.positive {
-    background-color: color("success", 500);
+    background: color("success", 500);
+    background: linear-gradient(
+      245deg,
+      color("success", 500) 0%,
+      color("success", 400) 100%
+    );
   }
+
   &.negative {
-    background-color: color("danger", 500);
+    background: color("danger", 500);
+    background: linear-gradient(
+      245deg,
+      color("danger", 500) 0%,
+      color("danger", 400) 100%
+    );
   }
 }
 </style>
