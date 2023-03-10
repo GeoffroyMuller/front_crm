3f2fbe75c041281ce356e6c98bcbb41307c1cd5e
geoff <geoffroym2@gmail.com>
2023-03-10 02:24:49 +0100

    up save global resa

--- packages/erp/src/pages/reservations/index.vue
+++ packages/erp/src/pages/reservations/index.vue
@@ -65,24 +65,29 @@
     <Sidebar v-model:open="isSidebarOpen">
       <div class="sidebar-reservation-form">
         <ReservationPrepareProductsReal
-          v-if="isPrepareProductsReal"
+          v-show="isPrepareProductsReal"
           :reservation="formDataReservation"
           @saved="
             (res: Reservation) => {
-              formDataReservation = res;
+              reservationSelected = res;
+              isPrepareProductsReal = false;
             }
           "
           @back="
             () => {
               isPrepareProductsReal = false;
+              formDataReservation = { ...reservationSelected } as Reservation;
             }
           "
         />
         <ReservationForm
-          v-if="(!reservationSelected || isEdit) && !isPrepareProductsReal"
+          v-show="(!reservationSelected || isEdit) && !isPrepareProductsReal"
           :initialReservation="reservationSelected"
           v-model:reservation="formDataReservation"
           @back="backToView"
+          @saved="(res: Reservation) => {
+              reservationSelected = res;
+            }"
           @prepare-products-real="
             () => {
               isPrepareProductsReal = true;
@@ -90,10 +95,10 @@
           "
         ></ReservationForm>
         <ReservationView
-          v-else-if="!isPrepareProductsReal && reservationSelected"
+          v-if="isView"
           @click-edit="
             (res) => {
-              reservationSelected = res;
+              formDataReservation = res;
               isEdit = true;
             }
           "
@@ -114,15 +119,12 @@ import Page from "@/components/Page.vue";
 import useReservationStore from "@/stores/reservations";
 import type { Reservation } from "@/types/reservation";
 import Sidebar from "core/src/components/Sidebar.vue";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 import ReservationForm from "@/components/reservations/ReservationForm.vue";
 import Button from "core/src/components/Button.vue";
 import ReservationView from "@/components/reservations/ReservationView.vue";
 import useUI from "core/src/composables/ui";
-import ReservationProductRealLine from "@/components/reservations/ReservationProductRealLine.vue";
 import ReservationPrepareProductsReal from "@/components/reservations/ReservationPrepareProductsReal.vue";
-
-import { isNil } from "lodash";
 
 const { toast, confirm } = useUI();
 
@@ -133,6 +135,12 @@ const reservationSelected = ref<Reservation | null>(null);
 const formDataReservation = ref<Reservation | null>(null);
 const isEdit = ref<boolean>(false);
 const isPrepareProductsReal = ref<boolean>(false);
+
+const isView = computed(() => {
+  return (
+    reservationSelected.value && !isEdit.value && !isPrepareProductsReal.value
+  );
+});
 
 watch(
   () => isSidebarOpen.value,
