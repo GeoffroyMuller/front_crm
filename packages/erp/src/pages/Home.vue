c7ccd5729544c33ddd9acf6dd3a804a065d77078
etienne1698 <etienne.robert1698@gmail.com>
2023-04-06 03:22:57 +0200

    up

--- packages/erp/src/pages/Home.vue
+++ packages/erp/src/pages/Home.vue
@@ -1,11 +1,6 @@
 <template>
-  <Page :title="$t('home')">
-    <PdfViewer
-      src="https://www.interieur.gouv.fr/content/download/32977/245942/file/cerfa_12669_01.pdf"
-    />
-  </Page>
+  <Page :title="$t('home')"> </Page>
 </template>
 <script setup lang="ts">
 import Page from "@/components/Page.vue";
-import PdfViewer from "core/src/components/PdfViewer.vue";
 </script>
