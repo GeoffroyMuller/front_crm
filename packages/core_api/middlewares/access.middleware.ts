8762916d933b25505701545b6b7b96f5c16c5633
etienne1698 <etienne.robert1698@gmail.com>
2023-04-19 21:43:54 +0200

    up

--- packages/core_api/middlewares/access.middleware.ts
+++ packages/core_api/middlewares/access.middleware.ts
@@ -10,18 +10,23 @@ export function accessMiddlewareFactory(rights: string | Array<string>) {
     next: NextFunction
   ) {
     if (req.auth.role == null) {
-        next();
-    } else {
-      const authRights =
-        req.auth?.role?.rights != null
-          ? JSON.parse(req.auth?.role?.rights)
-          : [];
-      for (const testedRight of rightsArray) {
-        if (!authRights.includes(testedRight)) {
-          return res.status(401).end();
-        }
-      }
       next();
+    } else {
+      try {
+        const authRights =
+          req.auth?.role?.rights != null
+            ? req.auth?.role?.rights
+            : [];
+        for (const testedRight of rightsArray) {
+          if (!authRights.includes(testedRight)) {
+            return res.status(401).end();
+          }
+        }
+        next();
+      } catch (err) {
+        console.error(err);
+        return res.status(401).end();
+      }
     }
   }
   return middleware;
