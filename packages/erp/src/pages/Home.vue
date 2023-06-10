c23ee534c0ad40999b4f8045350eafc30a6b323f
etienne1698 <etienne.robert1698@gmail.com>
2023-06-10 02:46:21 +0200

    style

--- packages/erp/src/pages/Home.vue
+++ packages/erp/src/pages/Home.vue
@@ -33,7 +33,11 @@
     <div class="p-4 bg-slate-800 rounded-lg">
       <div class="flex gap-2 mb-2 items-center" v-for="color of ['primary', 'danger', 'success', 'warning']">
           <span :class="`text-lg text-${color}-500 font-semibold w-[150px]`">{{ color }}</span>
-          <div v-for="i of [50,100,200,300,400,500,600,700,800,900]" :class="`bg-${color}-${i} w-12 h-12 rounded-md`" />
+          <div v-for="i of [50,100,200,300,400,500,600,700,800,900]" :class="`bg-${color}-${i} w-12 h-12 rounded-md grid place-items-center`">
+            <span class="text-white p-1 bg-slate-800 rounded-md">
+              {{ i }}
+            </span>
+          </div>
         </div>
     </div>
    
