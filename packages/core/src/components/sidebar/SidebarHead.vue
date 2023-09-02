0ca36bd84c241afc242b3504ffd9afb2b22f03a8
etienne1698 <etienne.robert1698@gmail.com>
2023-09-02 16:29:30 +0200

    sidebar head style

--- packages/core/src/components/sidebar/SidebarHead.vue
+++ packages/core/src/components/sidebar/SidebarHead.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="sticky top-0 left-0 w-full bg-white z-30">
-    <div class="flex items-center justify-between px-sidebarX py-4">
+  <div
+    class="sticky top-0 left-0 w-full bg-white z-30 border border-slate-200 border-solid border-l-0 border-r-0 border-t-0"
+  >
+    <div class="flex items-center justify-between px-sidebarX py-1">
       <div class="typo-title2">{{ title }}</div>
-      <IconButton name="close" size="xl" @click="handleClose" />
-    </div>
-    <div
-      class="flex items-center justify-between border border-slate-200 border-solid border-l-0 border-r-0 px-sidebarX py-2"
-    >
-      <div class="flex gap-4 items-center">
+      <div class="flex items-center gap-4 justify-end py-2">
         <template
           v-for="(a, index) of actions.filter((a) => a.main)"
           :key="a.title"
@@ -18,26 +15,23 @@
             variant="text"
             @click="() => handleAction(a)"
           >
-            {{ a.title }}
+            <span class="max-lg:hidden">{{ a.title }}</span>
           </Button>
           <div
             v-if="index < actions.filter((a) => a.main).length - 1"
-            class="h-[15px] border-primary-200 border-r border-solid border-l-0"
+            class="h-[15px] border-primary-200 border-r border-solid border-l-0 max-lg:hidden"
           />
         </template>
+
+        <ActionMenu
+          :actions="actions"
+          @action="($a) => emit('action', $a)"
+          placement="left-top"
+        >
+          <IconButton size="xl" name="more_horiz" default-colored />
+        </ActionMenu>
+        <IconButton name="close" size="xl" @click="handleClose" />
       </div>
-      <ActionMenu
-        :actions="actions"
-        @action="($a) => emit('action', $a)"
-        placement="left-top"
-      >
-        <IconButton
-          size="xl"
-          name="more_horiz"
-          color="primary"
-          default-colored
-        />
-      </ActionMenu>
     </div>
   </div>
 </template>
