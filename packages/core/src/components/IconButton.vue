aa5e3cde2f9d6857b0fad3e28dabcab7472bfea5
etienne1698 <etienne.robert1698@gmail.com>
2023-09-15 15:00:06 +0200

    icon-btn

--- packages/core/src/components/IconButton.vue
+++ packages/core/src/components/IconButton.vue
@@ -1,11 +1,6 @@
 <template>
-  <button
-    :disabled="disabled"
-    class="icon-button"
-    :class="`icon-button-${color} ${defaultColored ? 'default-colored' : ''}`"
-    type="button"
-  >
-    <Icon v-bind="iconProps" />
+  <button :disabled="disabled" type="button" class="icon-btn">
+    <Icon v-bind="iconProps" :color="color" class="relative" />
   </button>
 </template>
 <script setup lang="ts">
@@ -29,43 +24,17 @@ const iconProps = computed(() => {
   return p;
 });
 </script>
-<style lang="scss">
-.icon-button {
-  cursor: pointer;
-  user-select: none;
-  background-color: transparent;
-  width: fit-content;
-  height: fit-content;
-  border: none;
-  transition: background-color 0.15s ease;
-  padding: 0;
-  line-height: 0;
-  color: color("black");
-  &:disabled {
-    cursor: not-allowed;
-  }
-  @include media-up(md) {
-    &:hover:not(:disabled) {
-      color: color("primary", 500);
-    }
-  }
+
+<style>
+.icon-btn {
+  @apply cursor-pointer select-none bg-transparent block w-fit h-fit border-none transition-all p-0 leading-none disabled:cursor-not-allowed relative;
 }
-@each $key, $value in $colors {
-  @if type-of($value) == "map" {
-    .icon-button-#{$key} {
-      &.default-colored {
-        color: color($key, 500);
-      }
-      @include media-up(md) {
-        &:hover:not(:disabled) {
-          color: color($key, 600);
-        }
-      }
-    }
-  } @else {
-  }
+/* .icon-btn::before {
+  content: " ";
+  @apply absolute -top-[25%] -left-[25%] w-[150%] h-[150%] aspect-square rounded-full -z-10  bg-black opacity-0 transition-all duration-150;
 }
-.icon-button-white {
-  color: white;
-}
+.icon-btn:hover::before {
+  content: " ";
+  @apply opacity-[2%];
+} */
 </style>
