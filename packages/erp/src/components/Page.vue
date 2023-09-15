3f463a875113d5a215f0043b7921f8b541fd73f9
etienne1698 <etienne.robert1698@gmail.com>
2023-09-15 15:58:46 +0200

    delete useless

--- packages/erp/src/components/Page.vue
+++ packages/erp/src/components/Page.vue
@@ -1,32 +1,4 @@
 <template>
-  <!--   <div
-    class="page-menu"
-    :class="{ 'mini-nav': layoutSideMenu?.isNavMini?.value }"
-  >
-    <div class="typo-title2 text-white flex items-center h-full md:hidden">
-      <IconButton
-        v-if="back"
-        @click="goToBack"
-        name="chevron_left"
-        class="text-white hover:text-primary-500"
-      />
-      {{ title }}
-    </div>
-    <div class="typo-title2 max-md:hidden">
-      {{ title }}
-    </div>
-    <div class="buttons max-md-hidden">
-      <Media up="md">
-        
-      </Media>
-    </div>
-  </div> -->
-
-  <!--   <div class="page-btn-back" v-if="back">
-    <Button @click="goToBack" variant="text" icon="chevron_left">{{
-      $t("back")
-    }}</Button>
-  </div> -->
   <Spinner v-if="loading" class="page-spinner" />
 
   <div v-else class="page-container">
@@ -37,20 +9,8 @@
 </template>
 
 <script setup lang="ts">
-import Button from "core/src/components/Button.vue";
-import useUserStore from "@/stores/user";
-import { computed } from "vue";
 import Spinner from "core/src/components/Spinner.vue";
-import Media from "core/src/components/Media.vue";
-import ActionMenu from "core/src/components/ActionMenu.vue";
-import { useI18n } from "vue-i18n";
-import { useRouter } from "vue-router";
-import useUI from "core/src/composables/ui";
-import { inject } from "vue";
-import type { LayoutSideMenuProvide } from "core/src/layouts/types";
 import PageContent, { type PageContentProps } from "./PageContent.vue";
-import Avatar from "core/src/components/Avatar.vue";
-import IconButton from "core/src/components/IconButton.vue";
 
 interface PageProps {
   title: string;
@@ -60,31 +20,9 @@ interface PageProps {
   padding?: PageContentProps["padding"];
 }
 
-const props = withDefaults(defineProps<PageProps>(), {
+withDefaults(defineProps<PageProps>(), {
   padding: "light",
 });
-
-const layoutSideMenu = inject<LayoutSideMenuProvide>("LayoutSideMenu");
-
-const router = useRouter();
-const userStore = useUserStore();
-const { t } = useI18n();
-
-const { confirm } = useUI();
-
-const auth = computed(() => {
-  return userStore.getAuth;
-});
-const goToBack = () => {
-  history.back();
-};
-
-async function disconnect() {
-  if (await confirm(t("sure-disconnect"))) {
-    userStore.disconnect();
-    router.replace("/login");
-  }
-}
 </script>
 
 <style lang="scss">
@@ -126,58 +64,5 @@ $headerHeightMobile: $layoutSideHeaderHeightMobile;
   flex-direction: column;
   width: 100%;
   gap: spacing(2);
-}
-
-.page-menu {
-  position: fixed;
-  z-index: 21;
-  top: 0;
-  transition: width 0.3s ease;
-  width: 100%;
-  @include media_up(md) {
-    width: calc(100% - $navWidth);
-  }
-
-  &.mini-nav {
-    width: calc(100% - $miniNavWidth);
-  }
-
-  height: $header_height;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: spacing(3.5) spacing(2);
-  background-color: color("primary", 400);
-  background: linear-gradient(
-    95deg,
-    darken(color("primary", 400), 1%) 0%,
-    color("primary", 400) 100%
-  );
-
-  .typo-title2 {
-    color: white;
-  }
-
-  .buttons {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    gap: spacing(1.5);
-
-    .auth {
-      color: white;
-      margin-right: spacing(0.25);
-
-      &:hover {
-        text-shadow: unset;
-        color: color("primary", 100);
-      }
-    }
-  }
-}
-
-.page-btn-back {
-  padding: 0 spacing(2);
-  margin-top: spacing(2);
 }
 </style>
