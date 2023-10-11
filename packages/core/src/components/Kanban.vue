7dc5c8fed933b1bd7609b73a95d8055ae373c94b
etienne1698 <etienne.robert1698@gmail.com>
2023-10-12 01:48:05 +0200

    up

--- packages/core/src/components/Kanban.vue
+++ packages/core/src/components/Kanban.vue
@@ -1,21 +1,94 @@
 <template>
-  <div></div>
+  <div class="kanban">
+    <draggable
+      class="columns"
+      v-model="columns"
+      group="columns"
+      @start="handleDragStart"
+      @end="handleDragEnd"
+      item-key="id"
+      ghost-class="kanban-column-ghost"
+      handle=".drag_handle"
+      :forceFallback="true"
+      dragClass="kanban-column-cloned-element"
+    >
+      <template #item="{ element }">
+        <div class="column">
+          <div
+            class="flex items-center justify-between pl-4 pr-2 py-1 pb-4 typo-title4"
+          >
+            <div>
+              {{ element?.title || "" }}
+              <slot name="title" />
+            </div>
+            <Icon name="drag_indicator" class="drag_handle" />
+          </div>
+          <div class="px-2 grid gap-2">
+            <slot
+              v-for="card of element.elements"
+              :key="card.id"
+              name="element"
+              :element="card"
+            />
+          </div>
+        </div>
+      </template>
+    </draggable>
+  </div>
 </template>
 
-<script setup lang="ts" generic="T extends KanbanCard">
+<script setup lang="ts" generic="T">
 import type { ID } from "../types";
+import draggable from "vuedraggable";
+import Icon from "./Icon.vue";
+import { computed } from "vue";
 
-export interface KanbanCard {
+export interface KanbanColumns<X> {
   id: ID;
-  next: KanbanCard["id"];
+  title?: string;
+  elements: Array<X>;
 }
 
-export type kanbanProps<X extends KanbanCard> = {
-  cards: X[];
+export type kanbanProps<X> = {
+  columns: KanbanColumns<X>[];
 };
 
 const props = defineProps<kanbanProps<T>>();
 
+const emit = defineEmits(["update:columns"]);
 
-
+function handleDragStart() {
+  console.log("drag start");
+}
+function handleDragEnd() {
+  console.error("DRAG START");
+}
+const columns = computed({
+  get: () => props.columns,
+  set: (val) => {
+    emit("update:columns", val);
+  },
+});
 </script>
+
+<style>
+.kanban {
+  height: 100%;
+  .columns {
+    @apply h-full flex items-start gap-4;
+    .column {
+      @apply shadow-sm rounded min-w-[200px] h-full min-h-[200px] bg-white py-1;
+      .drag_handle {
+        cursor: grab;
+      }
+    }
+  }
+}
+.kanban-column-cloned-element {
+  opacity: 1 !important;
+  max-height: 200px !important;
+}
+.kanban-column-ghost {
+  opacity: 0.5 !important;
+}
+</style>
--- packages/core/src/components/demo/DemoKanban.vue
+++ packages/core/src/components/demo/DemoKanban.vue
@@ -1,12 +1,44 @@
 <template>
-  <Kanban :cards="cards" />
+  <Kanban v-model:columns="columns">
+    <template #element="{ element }">
+      <Card>
+        {{ element }}
+      </Card>
+    </template>
+  </Kanban>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import Kanban from "../Kanban.vue";
+import Kanban, { type KanbanColumns } from "../Kanban.vue";
+import Card from "../card/Card.vue";
 
-type DemoKanbanCard = { name: string; id: string; next: string };
+type DemoKanbanColmun = {} & KanbanColumns<any>;
 
-const cards = ref<DemoKanbanCard[]>([]);
+const columns = ref<DemoKanbanColmun[]>([
+  {
+    id: 0,
+    title: "A faire",
+    elements: [
+      {
+        id: 0,
+      },
+      {
+        id: 1,
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "En cours",
+    elements: [
+      {
+        id: 2,
+      },
+      {
+        id: 3,
+      },
+    ],
+  },
+]);
 </script>
