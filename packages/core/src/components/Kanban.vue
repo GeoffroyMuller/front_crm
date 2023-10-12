ff5bd6920253aad3f03af0e6f8a3c5e193c490b8
etienne1698 <etienne.robert1698@gmail.com>
2023-10-12 13:19:49 +0200

    up

--- packages/core/src/components/Kanban.vue
+++ packages/core/src/components/Kanban.vue
@@ -12,24 +12,35 @@
       :forceFallback="true"
       dragClass="kanban-column-cloned-element"
     >
-      <template #item="{ element }">
+      <template #item="{ element: column }">
         <div class="column">
           <div
             class="flex items-center justify-between pl-4 pr-2 py-1 pb-4 typo-title4"
           >
             <div>
-              {{ element?.title || "" }}
+              {{ column?.title || "" }}
               <slot name="title" />
             </div>
             <Icon name="drag_indicator" class="drag_handle" />
           </div>
-          <div class="px-2 grid gap-2">
-            <slot
-              v-for="card of element.elements"
-              :key="card.id"
-              name="element"
-              :element="card"
-            />
+          <div class="px-2">
+            <draggable
+              item-key="id"
+              group="elements"
+              :list="column.elements"
+              @start="handleDragStart"
+              @end="handleDragEnd"
+              handle=".drag_handle_element"
+              ghost-class="kanban-elem-ghost"
+              :forceFallback="true"
+              dragClass="kanban-elem-cloned-element"
+            >
+              <template #item="{ element }">
+                <div class="drag_handle_element cursor-grab mt-2">
+                  <slot :key="element.id" name="element" :element="element" />
+                </div>
+              </template>
+            </draggable>
           </div>
         </div>
       </template>
@@ -61,7 +72,7 @@ function handleDragStart() {
   console.log("drag start");
 }
 function handleDragEnd() {
-  console.error("DRAG START");
+  console.log("drag end");
 }
 const columns = computed({
   get: () => props.columns,
@@ -69,6 +80,9 @@ const columns = computed({
     emit("update:columns", val);
   },
 });
+
+// @ts-ignore
+window.columns = columns;
 </script>
 
 <style>
@@ -86,9 +100,23 @@ const columns = computed({
 }
 .kanban-column-cloned-element {
   opacity: 1 !important;
-  max-height: 200px !important;
+  max-height: 600px !important;
 }
 .kanban-column-ghost {
   opacity: 0.5 !important;
+}
+.kanban-elem-cloned-element {
+  opacity: 1 !important;
+  max-height: 600px !important;
+}
+.kanban-elem-ghost {
+  > * {
+    position: relative;
+    &::before {
+      content: " ";
+      border-radius: inherit;
+      @apply bg-slate-300 absolute top-0 left-0 w-full h-full;
+    }
+  }
 }
 </style>
