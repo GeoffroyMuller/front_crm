a4e7bd9220457d872266bb1247085bc236c932ec
etienne1698 <etienne.robert1698@gmail.com>
2023-12-03 00:09:59 +0100

    up

--- packages/erp/src/modules/billing/components/quotes/QuoteLine.vue
+++ packages/erp/src/modules/billing/components/quotes/QuoteLine.vue
@@ -22,7 +22,6 @@
           :label="$t('pages.edit-quote.product')"
           :store="productsStore"
           :get-option-label="(opt) => opt?.name"
-          multiple
           @update:selected="handleProductChange"
           option-key="id"
           name="idProduct"
