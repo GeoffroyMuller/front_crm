1c8e7a3fd413b12e6ff9cec3578d698dbcf75ea5
etienne1698 <etienne.robert1698@gmail.com>
2023-12-17 18:25:32 +0100

    up

--- packages/erp/src/modules/products/components/ProductGallery.vue
+++ packages/erp/src/modules/products/components/ProductGallery.vue
@@ -1,19 +1,34 @@
 <template>
   <div>
-    <img
-      class="w-full rounded"
-      v-if="product.images?.[0]"
-      :src="`${config.API_URL}/media/file/${product.images[0].filepath}`"
-    />
-    <div
-      class="grid grid-cols-3 gap-2 mt-2 [&>*]:w-full [&>*]:h-[100px] [&>*]:object-cover [&>*]:rounded"
-    >
+    <div class="relative group">
       <img
+        class="w-full object-cover h-[250px] rounded"
+        v-if="product.images?.[0]"
+        :src="`${config.API_URL}/media/file/${product.images[0].filepath}`"
+      />
+      <div
+        class="absolute top-1 right-1 items-center w-fit hidden group-hover:flex"
+      >
+        <IconButton name="delete" color="white" class="" />
+      </div>
+    </div>
+    <div class="grid grid-cols-3 gap-2 mt-2">
+      <div
+        class="relative group"
         v-for="image of product.images?.slice(1, 4)"
         :key="image.idMedia"
-        :src="`${config.API_URL}/media/file/${image.filepath}`"
-        alt=""
-      />
+      >
+        <img
+          :src="`${config.API_URL}/media/file/${image.filepath}`"
+          alt=""
+          class="object-cover h-[100px] w-full rounded"
+        />
+        <div
+          class="absolute top-1 right-1 items-center w-fit hidden group-hover:flex"
+        >
+          <IconButton name="delete" color="white" class="" />
+        </div>
+      </div>
     </div>
     <UploadMedia class="mt-4" @validate="async (data) => uploadImage(data)" />
   </div>
@@ -24,6 +39,7 @@ import UploadMedia from "@/components/UploadMedia.vue";
 import config from "@/const";
 import useProductsStore from "@/modules/products/stores/products";
 import type { Product } from "@/types/product";
+import IconButton from "core/src/components/IconButton.vue";
 
 const props = defineProps<{
   product: Product;
